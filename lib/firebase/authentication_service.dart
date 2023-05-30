48582bba4c4ee952e4e8fb67fa1a9e8b5b03ab04
jvcss <jvcs.mecatronica@gmail.com>
2023-05-30 14:34:31 -0300

    add Firebase workflow

---
+++ lib/firebase/authentication_service.dart
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 
+ * Implement the authentication_service.dart:
+ * 
+ *  This service will handle all the authentication-related 
+ * functionality using the Firebase Authentication SDK.
+ * 
+ *  It should provide methods for user registration, 
+ * user login, user logout, and user authentication state changes.
+ *
+ *  Use the methods provided by the Firebase Authentication SDK, 
+ * such as createUserWithEmailAndPassword,
+ *  signInWithEmailAndPassword, signOut, and authStateChanges, 
+ * to implement these functionalities.
+ */
